c364faaa50b598dbcb5d2df6a0d2efb4cf9a58a4
HaxiDenti <aldie.nightstar@gmail.com>
2022-02-02 13:07:19 +0200

    Supplier update

--- haxiLex.py
+++ haxiLex.py
@@ -1,56 +1,72 @@
 import re
 
 class Regex:
-    def __init__(self, name, t) -> None:
-        self.pattern = re.compile(t)
-        self.name = name
+    def __init__(self, pattern, supplierFn=None) -> None:
+        self.pattern = re.compile(pattern)
+        self.supplier = supplierFn
     def match(self, s):
-        return self.pattern.match(s)
+        m = self.pattern.match(s)
+        if m == None:
+            return None, None
+        if self.supplier != None:
+            return self.supplier(m), m
+        return None, m
+    
+def supplyGroup(n, name="undef"):
+    def supp(m):
+        try:
+            return (name, m.group(n))
+        except:
+            return None
+    return supp
 
 def decorPrefixes(*prefixes):
-	def deco(f):
-		def wrp(s):
-			for pref in prefixes:
-				if s.startswith(pref):
-					return f(s)
-			return None, 0
-		return wrp
-	return deco
+    def deco(f):
+        def wrp(s):
+            for pref in prefixes:
+                if s.startswith(pref):
+                    return f(s)
+            return None, 0
+        return wrp
+    return deco
 
 def lex(s, *lexers):
-	pos = 0
-	while True:
-		if pos >= len(s):
-			break
-		if s[pos] in " \t\r\n":
-			yield " ", 1
-			pos += 1
-			continue
-		res, cnt = lexOne(s[pos:], lexers)
-		if cnt < 1: # Unknown token
-			raise Exception(f"Unknown token at sym-pos {pos}: {s[pos:pos+10]}")
-		yield res, cnt
-		pos += cnt
+    pos = 0
+    while True:
+        if pos >= len(s):
+            break
+        if s[pos] in " \t\r\n":
+            yield " ", 1
+            pos += 1
+            continue
+        res, cnt = lexOne(s[pos:], lexers)
+        if cnt < 1: # Unknown token
+            raise Exception(f"Unknown token at sym-pos {pos}: {s[pos:pos+10]}")
+        yield res, cnt
+        pos += cnt
 
 def lexOne(string, lexers):
-	for lex in lexers:
-		if type(lex) is str:
-			lexStr = lex
-			def lx(s):
-				if s.startswith(lexStr):
-					return lexStr, len(lexStr)
-				return None, 0
-			lex=lx
-		elif type(lex) is Regex:
-			regex = lex
-			def lx(s):
-				m = regex.match(s)
-				if m == None:
-					return None, 0
-				return (regex.name, m), len(m.group(0))
-			lex = lx
-		res, cnt = lex(string)
-		if cnt < 1:
-			continue
-		return res, cnt
-	return None, 0
+    for lex in lexers:
+        if type(lex) is str:
+            lexStr = lex
+            def lx(s):
+                if s.startswith(lexStr):
+                    return lexStr, len(lexStr)
+                return None, 0
+            lex=lx
+        elif type(lex) is Regex:
+            regex = lex
+            def lx(s):
+                supplied, m = regex.match(s)
+                if m == None:
+                    return None, 0
+                LEN = len(m.group(0))
+                if supplied == None:
+                    return m, LEN
+                return supplied, LEN
+            lex = lx
+        res, cnt = lex(string)
+        if cnt < 1:
+            continue
+        return res, cnt
+    return None, 0
--- test.py
+++ test.py
@@ -3,50 +3,50 @@ import re
 
 # Simple lexer
 def abcLexer(s):
-	sb = []
-	for c in s:
-		if c in "abc":
-			sb.append(c)
-		else:
-			break
-	return "".join(sb), len(sb)
+    sb = []
+    for c in s:
+        if c in "abc":
+            sb.append(c)
+        else:
+            break
+    return "".join(sb), len(sb)
 
 def numLexer(s):
-	sb = []
-	for c in s:
-		if c in "01234567890":
-			sb.append(c)
-		else:
-			break
-	return "".join(sb), len(sb)
+    sb = []
+    for c in s:
+        if c in "01234567890":
+            sb.append(c)
+        else:
+            break
+    return "".join(sb), len(sb)
 
 # Let's create some real token
 class Tag:
-	def __init__(self, name):
-		self.name = name
-	def __repr__(self):
-		return f"Tag[{self.name}]"
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return f"Tag[{self.name}]"
 
 # This decor says that we parse only values which starts with: "#" or "@"
 # Better to make all prefixes same size. No need to verify then
 @decorPrefixes("#", "@")
 def tagLexer(s):
-	# This Lexer will return Tag(name) class.
-	sb = []
-	for c in s[1:]:
-		if c in " \t\r\n":
-			break
-		sb.append(c)
-	return Tag("".join(sb)), len(sb) + 1 # Why + 1? Because we started from s[1:] pos, and skipped first symbol.
+    # This Lexer will return Tag(name) class.
+    sb = []
+    for c in s[1:]:
+        if c in " \t\r\n":
+            break
+        sb.append(c)
+    return Tag("".join(sb)), len(sb) + 1 # Why + 1? Because we started from s[1:] pos, and skipped first symbol.
 
 
 # Now collect tokens
 for tok, cnt in lex("abc ab aa 123012 #ThisIsTag #ThisIsTag2 #Tag3 ccc 11 aa", abcLexer, numLexer, tagLexer):
-	# Let's skip the spaces
-	# All spaces converted to " "
-	if tok == " ": continue
-	# Now let's print actual tokens
-	print(tok, cnt)
+    # Let's skip the spaces
+    # All spaces converted to " "
+    if tok == " ": continue
+    # Now let's print actual tokens
+    print(tok, cnt)
 
 # Will print:
 #
@@ -66,7 +66,7 @@ for tok, cnt in lex("abc ab aa 123012 #ThisIsTag #ThisIsTag2 #Tag3 ccc 11 aa", a
 # If token not found, then tok == None and cnt == 0
 tok, cnt = lexOne("abcccbbba 123", [abcLexer, numLexer])
 if cnt > 0:
-	print("The token is: " + tok)
+    print("The token is: " + tok)
 
 # Will print:
 #
@@ -78,9 +78,9 @@ if cnt > 0:
 # This could help us to indentify couple of keywords without creating lexers
 # Let's say that words be: 'export', 'import', 'call'
 for tok, cnt in lex("export abbc call 11111", "export", "import", "call", abcLexer, numLexer):
-	# Skip all the spaces
-	if tok == " ": continue
-	print(tok)
+    # Skip all the spaces
+    if tok == " ": continue
+    print(tok)
 # Will output:
 #
 # export
@@ -89,26 +89,27 @@ for tok, cnt in lex("export abbc call 11111", "export", "import", "call", abcLex
 # 11111
 
 
-# Also we can try to use Regex(name, str) as an lexer
-# To identify token we need to check token is ("Tag", ...)
-for tok, cnt in lex("#Hello #Hi #HashTag3 123", Regex("Tag", "\\#([a-zA-Z0-9\\_]*)"), numLexer):
-	# Skip the spaces
-	if tok == " ": continue
-    # If token is tuple like ("Tag", ...)
-	if type(tok) is tuple and tok[0] == "Tag":
-		# Print out the token info
-		#   tok[1].group(0) - is all matched text
-		#   tok[1].group(1) - is a captured group by () in regexp
-		# so we getting tok.group(1) as in regexp we have ()
-		print("Token: ", tok[1].group(1))
-		continue
-	# Print the other tokens
-	print(tok)
+
+
+
+
+
+# Also we can try to use Regex(patternStr, supplyGroup(n, name)) as an lexer
+# To identify token we need to check token is ("TAG", ...)
+#     supplyGroup(1, "TAG") - allows as to parse matched value and get regex group, then converts to tuple with name
+for tok, cnt in lex("#Hello #Hi #HashTag3 123", Regex("\\#([a-zA-Z0-9\\_]*)", supplyGroup(1, "TAG")), numLexer):
+    # Skip the spaces
+    if tok == " ": continue
+    # Check the tags
+    if type(tok) is tuple and tok[0] == "TAG":
+        print("The tag: " + tok[1])
+    else:
+        print("Other token", tok)
 
 
 # This will print:
 #
-# Token:  Hello
-# Token:  Hi
-# Token:  HashTag3
-# 123
+# The tag: Hello
+# The tag: Hi
+# The tag: HashTag3
+# Other token 123
